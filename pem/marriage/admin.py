6fcadb9be2b12bf6b43da5d0bb1e9b51e0a06430
Lokesh Kumar Prajapati <133516405+LokeshKumarPrajapati@users.noreply.github.com>
2024-03-24 15:26:38 +0530

    final

---
+++ pem/marriage/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Stories, Services, Gallery,Testimonials,Contact,Gallery2,MasterImageTable,MasterVideoTable,IndexImageTable
+
+admin.site.register(Stories)
+admin.site.register(Services)
+admin.site.register(Gallery)
+admin.site.register(Testimonials)
+admin.site.register(Contact)
+admin.site.register(Gallery2)
+admin.site.register(MasterImageTable)
+admin.site.register(MasterVideoTable)
+admin.site.register(IndexImageTable)
+
+# Register your models here.
